bdeb1a9c3deb43f9d68d3655545ff747c1059eab
amkloss <hiw4tt@gmail.com>
2023-12-15 13:07:05 +0100

    Search terms music
    gemensamt överenskommna search terms

--- server.js
+++ server.js
@@ -22,17 +22,34 @@ const app = express();
 
 app.listen(3000, () => console.log("Server running on port 3000"));
 
-app.get("/music/:search", async (request, response) => {
-  // Make a database query and remember the result
-  let result = await query(`
-    SELECT *
-    FROM mainTable
-    WHERE filetype = '.mp3'  -- Get only mp3 files
-    AND LOWER(CONCAT(metadata->'$.common.album', ' ', metadata->'$.common.artist')) LIKE LOWER('%${request.params.search}%')
-  `);
+  app.get("/music/:search", async (request, response) => {
+    // Make a database query and remember the result
+    let result = await query(`
+      SELECT *
+      FROM mainTable
+      WHERE filetype = '.mp3'  -- Get only mp3 files
+      AND LOWER
+    (CONCAT(metadata->'$.common.album', 
+    ' ', 
+    metadata->'$.common.artist', 
+    ' ',
+    metadata->'$.common.track', 
+    ' ',
+    metadata->'$.common.year', 
+    ' ',
+    metadata->'$.common.duration',
+    ' ',
+    metadata->'$.common.URL')) 
+    LIKE LOWER('%${request.params.search}%')
+    `);
+ 
+
   // Send a response to the client
   response.json(result);
-  console.log("YOU ASKED FOR MUSIC", result)
+
+  //console.log("YOU ASKED FOR MUSIC", result)
 });
 
 //process.exit();
+
+
